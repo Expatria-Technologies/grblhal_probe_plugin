31736c8f300ae56559b80eb9bbe0c1242bab2b20
andrew_marles <andrew@expatria.ca>
2023-09-14 12:12:25 -0700

    Toolchanges working again.

--- probe_plugin.c
+++ probe_plugin.c
@@ -101,14 +101,14 @@ static probe_state_t prev_probe;
 
 static uint8_t probe_connect_port;
 static uint8_t tool_probe_port;
-static bool nvs_hardlimits, nvs_invert_probe_pin = false;
+static bool nvs_hardlimits, nvs_invert_probe_pin, protection_enabled = false;
 static probe_connected_flags_t probe_connected;
 static driver_reset_ptr driver_reset;
 static user_mcode_ptrs_t user_mcode;
 
 static nvs_address_t nvs_address;
 static on_report_options_ptr on_report_options;
-static probe_connected_toggle_ptr probe_connected_toggle;
+//static probe_connected_toggle_ptr probe_connected_toggle;
 static probe_protect_settings_t probe_protect_settings;
 static on_probe_start_ptr on_probe_start;
 static on_probe_completed_ptr on_probe_completed;
@@ -117,13 +117,32 @@ static on_spindle_select_ptr on_spindle_select;
 static stepper_pulse_start_ptr stepper_pulse_start;
 static spindle_set_state_ptr on_spindle_set_state = NULL;
 static on_tool_selected_ptr on_tool_selected = NULL;
+static on_tool_changed_ptr on_tool_changed = NULL; 
 static probe_get_state_ptr probe_get_state = NULL;
+
+static void set_connected_status(uint_fast16_t state);
+
+//returns true if probe is connected and sets core variable.
+static void check_connected_pin (void)
+{
+    uint8_t val;
+
+    if(probe_protect_settings.flags.ext_pin){
+        //hal.delay_ms(RELAY_DEBOUNCE, NULL); // Delay a bit to let any contact bounce settle.
+        val = hal.port.wait_on_input(Port_Digital, probe_connect_port, WaitMode_Immediate, 0.0f);//read the IO pin        
+        if(val == 1)
+            probe_connected.ext_pin = true;
+        else
+            probe_connected.ext_pin = false;
+
+        if(probe_protect_settings.flags.ext_pin_inv)
+            probe_connected.ext_pin = !probe_connected.ext_pin;
+    }
+}
 
 ISR_CODE static void set_connected (uint8_t irq_port, bool is_high)
 {
-    probe_state_t probe = hal.probe.get_state();
-    if (!probe.connected)
-        grbl.enqueue_realtime_command(CMD_PROBE_CONNECTED_TOGGLE);
+    protocol_enqueue_rt_command(set_connected_status);    
 }
 
 static user_mcode_t mcode_check (user_mcode_t mcode)
@@ -171,11 +190,10 @@ static probe_state_t probeGetState (void)
 static void protect_debounce_cb(void){
     probe_state_t probe = hal.probe.get_state();
 
-    if (probe.triggered || !probe.connected) { // Check probe state.  Alarm is still asserted.
+    if (probe.triggered) { // Check probe state.  Alarm if still asserted.
         grbl.enqueue_realtime_command(CMD_STOP);
         report_message("PROBE PROTECTED!", Message_Warning);
-    }
-    
+    }    
 }
 
 static void on_pulse_start (stepper_t *stepper){
@@ -183,12 +201,12 @@ static void on_pulse_start (stepper_t *stepper){
     probe_state_t probe = hal.probe.get_state();
 
     //if ((!prev_probe.triggered && probe.triggered) || !probe.connected) { // if probe has a rising edge from last pulse or is disconnected.
-    if ((!prev_probe.triggered && probe.triggered)) { // if probe has a rising edge from last pulse or is disconnected.
+    if ((!prev_probe.triggered && probe.triggered)) { // if probe has a rising edge from last pulse
         //call debounce callback
         hal.delay_ms(PROBE_DEBOUNCE, protect_debounce_cb);
     }
 
-    prev_probe = probe;
+    prev_probe.triggered = probe.triggered;
     
     if(stepper_pulse_start)
         stepper_pulse_start(stepper);
@@ -196,19 +214,28 @@ static void on_pulse_start (stepper_t *stepper){
 
 static void protection_on (void){
 
-    if (probe_protect_settings.flags.motion_protect){
+    if (!protection_enabled && probe_connected.value && probe_protect_settings.flags.motion_protect){
+        
+        protection_enabled = On;
+
         probe_state_t probe = hal.probe.get_state();
         prev_probe.triggered = probe.triggered;
+
         stepper_pulse_start = hal.stepper.pulse_start;
-        hal.stepper.pulse_start = on_pulse_start;
+        hal.stepper.pulse_start = on_pulse_start;   
+        //report_message("Protection On", Message_Info);             
     }
 }
 
 static void protection_off (void){
     
-    if(stepper_pulse_start){
-        hal.stepper.pulse_start = stepper_pulse_start;
+    if(protection_enabled && probe_protect_settings.flags.motion_protect){
+        protection_enabled = Off;
+
+        if(stepper_pulse_start)
+            hal.stepper.pulse_start = stepper_pulse_start;
         stepper_pulse_start = NULL;  //risk of null pointer error?
+        //report_message("Protection Off", Message_Info);        
     }
 }
 
@@ -224,10 +251,8 @@ static bool probe_start (axes_signals_t axes, float *target, plan_line_data_t *p
 }
 
 static void probe_completed (void){
-    //if probe connected, re-activate protection.
-    probe_state_t probe = hal.probe.get_state();
-    if (probe.connected && probe_connected.value && !gc_state.tool_change)
-        protection_on();
+    //re-activate protection.
+    protection_on();
 
     //if probe state was redirected, restore it
     if(probe_get_state){
@@ -239,6 +264,19 @@ static void probe_completed (void){
         on_probe_completed();
 }
 
+static void tool_changed (tool_data_t *tool){    
+    //tool_change.c sets grbl.on_probe_completed to NULL when finished.  Is this correct?  That breaks the call chain in this plugin.
+    //restore the pointer here.
+    
+    if(grbl.on_probe_completed == NULL){
+        on_probe_completed = grbl.on_probe_completed;
+        grbl.on_probe_completed = probe_completed;
+    }
+
+    //continue call chain
+    if(on_tool_changed)
+        on_tool_changed(tool);
+} 
 
 //The grbl.on_probe_fixture event handler is called by the default tool change algorithm when probing at G59.3.
 //In addition it will be called on a "normal" probe sequence if the XY position is
@@ -248,12 +286,10 @@ static void probe_completed (void){
 bool probe_fixture (tool_data_t *tool, bool at_g59_3, bool on)
 {
     bool status = true;
-    probe_state_t probe = hal.probe.get_state();
 
-    if(tool){ //are doing a tool change.
-
-        //always disable motion protection when probing the fixture
-        protection_off();
+    if(on){ //are doing a tool change.
+        
+        protection_off();  //disable protection when probing
 
         //set polarity before probing the fixture.
         if(probe_protect_settings.flags.invert)
@@ -261,23 +297,20 @@ bool probe_fixture (tool_data_t *tool, bool at_g59_3, bool on)
         
         //if a different pin is configured, re-direct probe reading to that pin via function pointer.
         if(probe_protect_settings.flags.tool_pin){
-            //store current probe state
+            //store current probe state function
             probe_get_state = hal.probe.get_state;
             hal.probe.get_state = probeGetState;
         }
 
         //set hard limits before probing the fixture.
-        if(tool && !nvs_hardlimits && probe_protect_settings.flags.hardlimits){ //if the hard limits are not already enabled they need to be enabled.
+        if(!nvs_hardlimits && probe_protect_settings.flags.hardlimits){ //if the hard limits are not already enabled they need to be enabled.
             hal.limits.enable(settings.limits.flags.hard_enabled, true); // Change immediately. NOTE: Nice to have but could be problematic later.
         }
-        hal.delay_ms(RELAY_DEBOUNCE, NULL); // Delay a bit to let any contact bounce settle.
-    } else {
-        //restore anything changed during tool probing.
-        settings.probe.invert_probe_pin = nvs_invert_probe_pin;
+    }else{
+        settings.probe.invert_probe_pin = nvs_invert_probe_pin;  //restore pin inversion setting
         hal.limits.enable(settings.limits.flags.hard_enabled, nvs_hardlimits);  //restore hard limit settings.
-        if (probe.connected && probe_connected.value)
-            protection_on();
-    }    
+        protection_on();      //restore protection.   
+    }
 
     if(on_probe_fixture)
         status = on_probe_fixture(tool, at_g59_3, on);
@@ -285,24 +318,14 @@ bool probe_fixture (tool_data_t *tool, bool at_g59_3, bool on)
     return status;
 }
 
-static void on_probe_connected_toggle(void){
+//static void on_probe_connected_toggle(void){
+static void set_connected_status(uint_fast16_t state){    
     
-    int val = 0;
     static uint8_t previous_flags;
-    //if there is a pin, read it
-    if(probe_protect_settings.flags.ext_pin){
-        //hal.delay_ms(RELAY_DEBOUNCE, NULL); // Delay a bit to let any contact bounce settle.
-        val = hal.port.wait_on_input(Port_Digital, probe_connect_port, WaitMode_Immediate, 0.0f);//read the IO pin        
-        if(val == 1)
-            probe_connected.ext_pin = true;
-        else
-            probe_connected.ext_pin = false;
+    //probe_state_t probe = hal.probe.get_state();
 
-        if(probe_protect_settings.flags.ext_pin_inv)
-            probe_connected.ext_pin = !probe_connected.ext_pin;
-    }
+    check_connected_pin();
 
-    
     if(probe_connected.ext_pin)
         report_message("External Probe connected!", Message_Info);    
 
@@ -314,24 +337,16 @@ static void on_probe_connected_toggle(void){
 
     if(probe_connected.toggle)
             report_message("Probe connect toggled on", Message_Info);
-
-    if(probe_connected.value)
-        protection_on();
-    else{
-        protection_off();
+    
+    if(probe_connected.value){
+        protection_on(); 
+    } else{
+        protection_off(); 
         if (previous_flags != probe_connected.value)
             report_message("Probe disconnected, protection off.", Message_Info);
     }
 
-    previous_flags = probe_connected.value;
-
-    probe_state_t probe = hal.probe.get_state();
-
-    if(!probe.connected) {
-        if(probe_connected_toggle)
-            probe_connected_toggle();        
-    }
-
+    previous_flags = probe_connected.value;   
 }
 
 static void onSpindleSetState (spindle_state_t state, float rpm)
@@ -342,7 +357,6 @@ static void onSpindleSetState (spindle_state_t state, float rpm)
         grbl.enqueue_realtime_command(CMD_RESET);
         report_message("PROBE IS IN SPINDLE!", Message_Warning);
     }
-
     on_spindle_set_state(state, rpm);
 }
 
@@ -356,15 +370,17 @@ static bool onSpindleSelect (spindle_ptrs_t *spindle)
 
 static void onToolSelected (tool_data_t *tool)
 {
+    //probe_state_t probe = hal.probe.get_state();
     //if the tool is 99, set probe connected.
     current_tool = tool;
 
-    if (tool->tool_id == 99)
-        probe_connected.t99 = true;
-    else
-        probe_connected.t99 = false;
-    
-    on_probe_connected_toggle();
+    if (tool->tool_id == 99){
+        probe_connected.t99 = true;     
+    }else {
+        probe_connected.t99 = false;        
+    }
+
+    set_connected_status(tool->tool_id);
 
     if(on_tool_selected)
         on_tool_selected(tool);
@@ -373,18 +389,14 @@ static void onToolSelected (tool_data_t *tool)
 static void mcode_execute (uint_fast16_t state, parser_block_t *gc_block)
 {
     bool handled = true;
-    probe_state_t probe = hal.probe.get_state();
+    //probe_state_t probe = hal.probe.get_state();
 
     if (state != STATE_CHECK_MODE)
       switch((uint16_t)gc_block->user_mcode) {
 
         case 401:
             if(!probe_connected.mcode){
-                probe_connected.mcode = true;
-                //enqueue probe connected symbol.                
-                if (!probe.connected)                
-                    grbl.enqueue_realtime_command(CMD_PROBE_CONNECTED_TOGGLE);
-                hal.delay_ms(RELAY_DEBOUNCE, NULL); // Delay a bit to let any contact bounce settle.
+                probe_connected.mcode = true;               
             }else
                 report_message("Probe connected signal already asserted!", Message_Warning);
             break;
@@ -392,10 +404,6 @@ static void mcode_execute (uint_fast16_t state, parser_block_t *gc_block)
         case 402:
             if(probe_connected.mcode){
                 probe_connected.mcode = false;
-                //enqueue probe disconnected symbol.
-                if (probe.connected)                
-                    grbl.enqueue_realtime_command(CMD_PROBE_CONNECTED_TOGGLE);
-                hal.delay_ms(RELAY_DEBOUNCE, NULL); // Delay a bit to let any contact bounce settle.
             }else
                 report_message("Probe connected signal not asserted!", Message_Warning);
             break;
@@ -404,6 +412,8 @@ static void mcode_execute (uint_fast16_t state, parser_block_t *gc_block)
             handled = false;
             break;
     }
+
+    set_connected_status(probe_connected.mcode);  
 
     if(!handled && user_mcode.execute)
         user_mcode.execute(state, gc_block);
@@ -414,6 +424,10 @@ static void probe_reset (void)
     settings.probe.invert_probe_pin = nvs_invert_probe_pin;
     hal.limits.enable(settings.limits.flags.hard_enabled, nvs_hardlimits);  //restore hard limit settings.
     //probe_connected.value = 0;  //seems like it is best for this to survive reset.
+    //protocol_enqueue_rt_command(set_connected_status);
+    probe_state_t probe = hal.probe.get_state();
+    if (!probe.connected)
+        grbl.enqueue_realtime_command(CMD_PROBE_CONNECTED_TOGGLE);      
     driver_reset();
 }
 
@@ -423,9 +437,7 @@ static void report_options (bool newopt)
 
     if(!newopt) {
         hal.stream.write("[PLUGIN:Probe Protection v0.01]" ASCII_EOL);
-    }        
-
-
+    }       
 }
 
 static void warning_msg (uint_fast16_t state)
@@ -442,7 +454,7 @@ static const setting_group_detail_t user_groups [] = {
 static const setting_detail_t user_settings[] = {
     { PROBE_PLUGIN_PORT_SETTING1, Group_Probing, "Probe Connected Aux Input", NULL, Format_Int8, "#0", "0", max_port, Setting_NonCore, &probe_protect_settings.protect_port, NULL, NULL },
     { PROBE_PLUGIN_PORT_SETTING2, Group_Probing, "Tool Probe Aux Input", NULL, Format_Int8, "#0", "0", max_port, Setting_NonCore, &probe_protect_settings.tool_port, NULL, NULL },    
-    { PROBE_PLUGIN_FIXTURE_INVERT_LIMIT_SETTING, Group_Probing, "Probe Protection Flags", NULL, Format_Bitfield, "Invert Tool Probe,Hard Limits, External Connected Pin, Invert External Connected Pin, Alternate Tool Probe Pin, Invert Tool Probe Pin, Enable Motion Protection", NULL, NULL, Setting_NonCore, &probe_protect_settings.flags, NULL, NULL },   
+    { PROBE_PLUGIN_FIXTURE_INVERT_LIMIT_SETTING, Group_Probing, "Probe Protection Flags", NULL, Format_Bitfield, "Invert Tool Probe, Tool Hard Limits, External Connected Pin, Invert External Connected Pin, Alternate Tool Probe Pin, Invert Tool Probe Pin, Enable Motion Protection", NULL, NULL, Setting_NonCore, &probe_protect_settings.flags, NULL, NULL },   
 };
 
 #ifndef NO_SETTINGS_DESCRIPTIONS
@@ -507,11 +519,10 @@ static void plugin_settings_load (void)
     nvs_hardlimits = settings.limits.flags.hard_enabled;
     nvs_invert_probe_pin = settings.probe.invert_probe_pin;
 
+    memcpy(&user_mcode, &hal.user_mcode, sizeof(user_mcode_ptrs_t));
+
     if(probe_protect_settings.flags.ext_pin){
         if(ioport_claim(Port_Digital, Port_Input, &probe_connect_port, "Probe Connected")) {
-
-            memcpy(&user_mcode, &hal.user_mcode, sizeof(user_mcode_ptrs_t));
-
         } else
             protocol_enqueue_rt_command(warning_no_port);    
 
@@ -522,12 +533,8 @@ static void plugin_settings_load (void)
 
     if(probe_protect_settings.flags.tool_pin){
         if(ioport_claim(Port_Digital, Port_Input, &tool_probe_port, "Toolsetter G59.3")) {
-
-            memcpy(&user_mcode, &hal.user_mcode, sizeof(user_mcode_ptrs_t));
-
         } else
-            protocol_enqueue_rt_command(warning_no_port);      
-
+            protocol_enqueue_rt_command(warning_no_port);    
         //Not an interrupt pin.
     }
 }
@@ -553,8 +560,8 @@ void probe_protect_init (void)
     probe_connected.value = 0;
 
     //Register function pointers
-    probe_connected_toggle = hal.probe.connected_toggle;
-    hal.probe.connected_toggle = on_probe_connected_toggle;
+    on_tool_changed = grbl.on_tool_changed;
+    grbl.on_tool_changed = tool_changed;
 
     on_probe_fixture = grbl.on_probe_fixture;
     grbl.on_probe_fixture = probe_fixture;
@@ -610,13 +617,9 @@ void probe_protect_init (void)
     }
 
     //need to add some init code to check the pin states (if enabled) and set the connected flag on or off.
-    /*
     probe_state_t probe = hal.probe.get_state();
-
-    if(probe.connected) {
-        if(probe_connected_toggle)
-            probe_connected_toggle();        
-    }*/
+    if (!probe.connected)
+        grbl.enqueue_realtime_command(CMD_PROBE_CONNECTED_TOGGLE);  
 
     if(!ok)
         protocol_enqueue_rt_command(warning_msg);
